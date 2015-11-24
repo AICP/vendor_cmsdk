CyanogenMod/cm_platform_sdk

Protected apps components
Change-Id: I850d45e9aca6eff8221766830fce45468c8ca549

src/java/cyanogenmod/content/Intent.java
@@ -40,4 +40,15 @@ public class Intent {
      */
     public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
             "cyanogenmod.intent.extra.RECENTS_LONG_PRESS_RELEASE";
+
+    /**
+     * Protected Apps
+     */
+    public static final String PROTECTED_ACTION = "cyanogenmod.intent.action.PACKAGE_PROTECTED";
+    public static final String PROTECTED_CHANGED_ACTION =
+            "cyanogenmod.intent.action.PROTECTED_COMPONENT_UPDATE";
+    public static final String PROTECTED_STATE =
+            "cyanogenmod.intent.action.PACKAGE_PROTECTED_STATE";
+    public static final String PROTECTED_COMPONENTS =
+            "cyanogenmod.intent.action.PACKAGE_PROTECTED_COMPONENTS";
 }

AICP/vendor_cmsdk

src/java/cyanogenmod/content/Intent.java
@@ -40,4 +40,29 @@ public class Intent {
      */
     public static final String EXTRA_RECENTS_LONG_PRESS_RELEASE =
             "cyanogenmod.intent.extra.RECENTS_LONG_PRESS_RELEASE";
+
+    /**
+     * Intent filter to update protected app component's settings
+     */
+    public static final String ACTION_PROTECTED = "cyanogenmod.intent.action.PACKAGE_PROTECTED";
+
+    /**
+     * Intent filter to notify change in state of protected application.
+     */
+    public static final String ACTION_PROTECTED_CHANGED =
+            "cyanogenmod.intent.action.PROTECTED_COMPONENT_UPDATE";
+
+    /**
+     * This field is part of the intent {@link #ACTION_PROTECTED_CHANGED}.
+     * Intent extra field for the state of protected application
+     */
+    public static final String EXTRA_PROTECTED_STATE =
+            "cyanogenmod.intent.extra.PACKAGE_PROTECTED_STATE";
+
+    /**
+     * This field is part of the intent {@link #ACTION_PROTECTED_CHANGED}.
+     * Intent extra field to indicate protected component value
+     */
+    public static final String EXTRA_PROTECTED_COMPONENTS =
+            "cyanogenmod.intent.extra.PACKAGE_PROTECTED_COMPONENTS";
 }