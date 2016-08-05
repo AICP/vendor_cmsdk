c82d693d952645ab62c81cb2d1807ab55b62fbe3
Davor Bertovic <davor@losinj.com>
2016-08-05 21:04:29 +0200

    Revert "cmsdk/livedisplay: Add support for picture adjustment"
    This reverts commit 72da93a7b1b7a9ab11b4234c4ae8d552ac3302b7.
    
    Change-Id: I07199e1fc497ce2ba0047b11b91452709ffb5278

--- sdk/src/java/cyanogenmod/providers/CMSettings.java
+++ sdk/src/java/cyanogenmod/providers/CMSettings.java
@@ -1825,33 +1825,6 @@ public final class CMSettings {
                 sBooleanValidator;
 
         /**
-         * The current custom picture adjustment values as a delimited string
-         */
-        public static final String DISPLAY_PICTURE_ADJUSTMENT =
-                "display_picture_adjustment";
-
-        /** @hide */
-        public static final Validator DISPLAY_PICTURE_ADJUSTMENT_VALIDATOR =
-                new Validator() {
-                    @Override
-                    public boolean validate(String value) {
-                        if (TextUtils.isEmpty(value)) {
-                            return true;
-                        }
-                        final String[] sp = TextUtils.split(value, "\\|");
-                        if (sp.length != 4 && sp.length != 5) {
-                            return false;
-                        }
-                        for (String s : sp) {
-                            if (Float.parseFloat(s) == Float.NaN) {
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-                };
-
-        /**
          * I can haz more bukkits
          * @hide
          */
@@ -2114,8 +2087,6 @@ public final class CMSettings {
                     NOTIFICATION_LIGHT_COLOR_AUTO_VALIDATOR);
             VALIDATORS.put(TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK,
                     TOUCHSCREEN_GESTURE_HAPTIC_FEEDBACK_VALIDATOR);
-            VALIDATORS.put(DISPLAY_PICTURE_ADJUSTMENT,
-                    DISPLAY_PICTURE_ADJUSTMENT_VALIDATOR);
             VALIDATORS.put(__MAGICAL_TEST_PASSING_ENABLER,
                     __MAGICAL_TEST_PASSING_ENABLER_VALIDATOR);
         };
