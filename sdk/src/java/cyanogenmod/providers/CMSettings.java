dd61e629f0832e5489d4d6ffb9e2977f3c9becb4
Ali B <abittin@gmail.com>
2016-08-19 13:07:46 +0300

    cmsdk: add advanced data tile
    Change-Id: I78c8487d1fe04c367e82158c566155d9e4d66225

--- sdk/src/java/cyanogenmod/providers/CMSettings.java
+++ sdk/src/java/cyanogenmod/providers/CMSettings.java
@@ -2728,6 +2728,12 @@ public final class CMSettings {
         public static final String QS_LOCATION_ADVANCED = "qs_location_advanced";
 
         /**
+         * Whether detail view for the data tile is enabled
+         * @hide
+         */
+        public static final String QS_DATA_ADVANCED = "qs_data_advanced";
+
+        /**
          * Whether to show the keyguard visualizer.
          * Boolean setting. 0 = off, 1 = on.
          * @hide
@@ -2846,6 +2852,7 @@ public final class CMSettings {
                 CMSettings.Secure.PERFORMANCE_PROFILE,
                 CMSettings.Secure.APP_PERFORMANCE_PROFILES_ENABLED,
                 CMSettings.Secure.QS_LOCATION_ADVANCED,
+                CMSettings.Secure.QS_DATA_ADVANCED,
                 CMSettings.Secure.LOCKSCREEN_VISUALIZER_ENABLED,
                 CMSettings.Secure.LOCK_PASS_TO_SECURITY_VIEW
         };
